d8192ca2bcc99f97fe8e71d7715030dc245211b2
kornfleks <a.nicolas.dev@gmail.com>
2020-09-26 19:27:01 +0200

    Add toDoItems offline persistance

--- src/components/todo.js
+++ src/components/todo.js
@@ -2,9 +2,23 @@ import * as React from 'react';
 import ToDoInput from './todoInput';
 import TodoList from './toDoList';
 
-export default function () {
+const STORAGE_KEY = '@react-pwa/todos';
 
-    const [toDoItems, setItems] = React.useState([]);
+const loadToDoItems = () => {
+    const data = localStorage.getItem(STORAGE_KEY);
+    if (!data) return [];
+    return JSON.parse(data);
+}
+
+const saveTodoItems = toDoItems => {
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(toDoItems));
+}
+
+export default function () {
+    const [toDoItems, setItems] = React.useState(loadToDoItems);
+
+    React.useEffect(() => saveTodoItems(toDoItems), [toDoItems]);
+
     const onAdd = (text) => setItems([...toDoItems, text]);
 
     return (
