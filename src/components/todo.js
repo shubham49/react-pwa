f89829db6c846636dec35178fed2bf3c17ac0f63
Angela Melinda <angela.melinda@gmail.com>
2020-09-26 18:03:39 +0700

    Add checkbox for completing task

--- src/components/todo.js
+++ src/components/todo.js
@@ -5,11 +5,18 @@ import TodoList from './toDoList';
 export default function () {
 
     const [toDoItems, setItems] = React.useState([]);
-    const onAdd = (text) => setItems([...toDoItems, text]);
+    const onAdd = (toDo) => setItems([...toDoItems, toDo]);
+    const onCheck = (toDo) => {
+        const newTodo = toDoItems.map((item) => 
+            item.id === toDo.id ? { ...item, complete: !item.complete } : item
+        )
+
+        setItems([ ...newTodo ])
+    }
 
     return (
         <>
-            <TodoList toDoItems={toDoItems} />
+            <TodoList toDoItems={toDoItems} onCheck={onCheck} />
             <ToDoInput onAdd={onAdd} />
         </>
     );
