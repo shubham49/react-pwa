fb96742d7cd6195a431be7a47a2caee90f331d73
shubham.garg <Ggn@4321>
2020-10-06 17:46:07 +0530

    adding checkbox

--- src/components/todoItem.js
+++ src/components/todoItem.js
@@ -3,10 +3,23 @@ import toDoStyles from './todo.css';
 
 export default function (props) {
   const items = props.items.split('\n');
+  const [isCompleted, setCompleted] = React.useState(false);
+
+  const onChange = (e) => 
+    setCompleted(e.target.checked);
+
   return (
     <div className={toDoStyles.todoItem}>
       {items.map(item => (
-        <div>{item}</div>
+        <div>
+          <input type="checkbox"
+            onChange={onChange}          
+          />
+          <span className={toDoStyles.item}>
+          {isCompleted && <del>{item}</del>}
+          {!isCompleted && item}
+          </span>
+        </div>
       ))}
     </div>
   );
