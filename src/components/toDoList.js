60a22544cdd5fc7ee09fb7b0baf2143f65a90002
shubham garg <gargshubham49@gmail.com>
2020-10-20 23:06:18 +0530

    add delete feature (#19)
    Co-authored-by: shubham.garg <shubham.garg@thoughtworks.com>

--- src/components/toDoList.js
+++ src/components/toDoList.js
@@ -2,11 +2,11 @@ import React from 'react';
 import toDoStyles from './todo.css';
 import ToDoItem from './todoItem';
 
-export default function ({ toDoItems }) {
+export default function ({ toDoItems, onDelete }) {
   return (
     <div className={`${toDoStyles.container} container`}>
       {toDoItems.map(items => (
-        <ToDoItem items={items} />
+        <ToDoItem items={items} onDelete={onDelete} />
       ))}
     </div>
   );
